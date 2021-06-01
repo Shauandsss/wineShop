02543823a265591bf4c7999ddacae68d6554e61f
shauandsss <shauands@gmail.com>
2021-05-31 21:27:08 -0300

    Updates

---
+++ precache-manifest.4e9af51154fc3c076848d4a0f5cde9d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd403c3f23dc9a897379",
+    "url": "/wine-shop/static/css/main.95ab1efc.chunk.css"
+  },
+  {
+    "revision": "fd403c3f23dc9a897379",
+    "url": "/wine-shop/static/js/main.f9a6f8a1.chunk.js"
+  },
+  {
+    "revision": "bee9095a165654579b64",
+    "url": "/wine-shop/static/js/runtime~main.8e57e9e5.js"
+  },
+  {
+    "revision": "a98722d0f88fe2e36d20",
+    "url": "/wine-shop/static/js/2.0bb2f578.chunk.js"
+  },
+  {
+    "revision": "ca565618a79b0cac2f4dcc5ed86df53b",
+    "url": "/wine-shop/static/media/WineComFundo.ca565618.jpg"
+  },
+  {
+    "revision": "62aad40eb0d0dac22bf3a79d4ce14bc8",
+    "url": "/wine-shop/static/media/WineSemFundo.62aad40e.png"
+  },
+  {
+    "revision": "d1b6347a7f9c676307ce46a779af60e4",
+    "url": "/wine-shop/static/media/BarrilComFundo.d1b6347a.jpg"
+  },
+  {
+    "revision": "82b9ef798500c16ae58a4b25dd12cb50",
+    "url": "/wine-shop/static/media/BarrilSemFundo.82b9ef79.png"
+  },
+  {
+    "revision": "63b9862eb387035c0ef906c8468971c9",
+    "url": "/wine-shop/index.html"
+  }
+];
